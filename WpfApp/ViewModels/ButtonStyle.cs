f28e186e19cb89bcd42916e2a388a189e67cbb3c
00-00263683 <d.matros@outlook.com>
2022-01-10 21:37:29 +0300

    Добавьте файлы проекта.

---
+++ WpfApp/ViewModels/ButtonStyle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp.ViewModels
+{
+    public static class ButtonStyle //Изменение названия и цвета границы кнопки
+    {
+        //Название:
+
+        public static string GetText(DependencyObject obj)
+            => (string)obj.GetValue(TextProperty);
+
+        public static void SetText(DependencyObject obj, string value)
+            => obj.SetValue(TextProperty, value);
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.RegisterAttached("Text", typeof(string),
+                typeof(ButtonStyle), new PropertyMetadata(""));
+
+        // Цвет границы:
+
+        public static string GetStroke(DependencyObject obj)
+            => (string)obj.GetValue(StrokeProperty);
+
+        public static void SetStroke(DependencyObject obj, string value)
+            => obj.SetValue(StrokeProperty, value);
+
+        public static readonly DependencyProperty StrokeProperty =
+            DependencyProperty.RegisterAttached("Stroke", typeof(string),
+                typeof(ButtonStyle), new PropertyMetadata(""));
+
+        // Цвет залики:
+
+        public static string GetFill(DependencyObject obj)
+            => (string)obj.GetValue(StrokeProperty);
+
+        public static void SetFill(DependencyObject obj, string value)
+            => obj.SetValue(FillProperty, value);
+
+        public static readonly DependencyProperty FillProperty =
+            DependencyProperty.RegisterAttached("Fill", typeof(string),
+                typeof(ButtonStyle), new PropertyMetadata(""));
+    }
+}
---
+++ WpfApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Window;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window = this;
+
+            foreach (UIElement el in GroupButton.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += ButtonClick;
+                }
+            }
+        }
+
+        // Головной блок
+        private void Exit(object sender, RoutedEventArgs e) //Закрыть окно
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Drag(object sender, RoutedEventArgs e) //Перетаскивание окна
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                MainWindow.Window.DragMove();
+            }
+        }
+        // Головной блок
+
+        private void ButtonClick(Object sender, RoutedEventArgs e)
+        {
+            string textButton = ((Button)e.OriginalSource).Content.ToString();
+            if (textButton == "C")  //Очистка окна от текста
+            {
+                text_1.Clear();
+                text_2.Clear();
+            }
+            else if (textButton == "CE") //Стирание текста по 1-му знаку
+            {
+                text_1.Text = text_1.Text.Remove(text_1.Text.Length - 1);
+                {
+                    if (text_1.Text.Length - 1 < 0)
+                    {
+                        text_1.Text = "Нажми 'С'!";
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+            else if (textButton == "=") // Расчет
+            {
+                text_2.Text = new DataTable().Compute(text_1.Text, null).ToString();
+            }
+            else
+            {
+                text_1.Text += textButton;
+            }
+
+        }
+    }
+}
